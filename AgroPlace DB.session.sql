fd11340569f68a324c9b79bf46d6b0ba447429e3
Ritesh Nagtilak <nagtilakritesh@gmail.com>
2025-08-05 20:19:17 +0530

    Removed sensitive SQL dump file

--- AgroPlace DB.session.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (id, username, email, password, whatsapp, role)
-VALUES (
-    id:int,
-    'username:varchar',
-    'email:varchar',
-    'password:varchar',
-    'whatsapp:varchar',
-    'role:enum'
-  );
